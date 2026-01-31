0eb28c24c4c815a7f135fd1c8ab21b30b6c4c935
Nestor Ibarra <nestoribarra@iMac-de-Nestor.local>
2026-01-31 11:41:15 -0500

    agregando el codigo de google analytics3

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ export default function RootLayout({
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-1E8CEW78BM"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
-          
+
           gtag('config', 'G-1E8CEW78BM');
         </script>
         {/* Google Tag Manager */}
