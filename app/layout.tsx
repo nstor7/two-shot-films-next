9c38ebb18c67dd9135b7a465bcf9a8fbf9a8bdd8
Nestor Ibarra <nestoribarra@iMac-de-Nestor.local>
2026-02-02 08:52:04 -0500

    cambiando el tag2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
 import "./globals.css";
 import Header from './components/header'
 import Footer from './components/footer'
+import { Metadata } from "next";
 
+export const metadata:Metadata = {
+  verification: {
+    google:  `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+           })(window,document,'script','dataLayer','GTM-543LHCXC');</script>`
+  }
+}
 
 export default function RootLayout({
   children,
@@ -11,14 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-1E8CEW78BM"></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-1E8CEW78BM');`}
-        </script> */}
         {/* Google Tag Manager */}
            <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
