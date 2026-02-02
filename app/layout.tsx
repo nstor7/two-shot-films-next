afc86277d58c7aae6e1fa05d20fef419942bb193
Nestor Ibarra <nestoribarra@iMac-de-Nestor.local>
2026-02-02 09:02:54 -0500

    cambiando el tag3

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,7 @@ import { Metadata } from "next";
 
 export const metadata:Metadata = {
   verification: {
-    google:  `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-           })(window,document,'script','dataLayer','GTM-543LHCXC');</script>`
+    google:  "-6zSAxAkd5NlnHJ6hqgMee2l7hOQZIYraJQT7TJkg6k"
   }
 }
 
